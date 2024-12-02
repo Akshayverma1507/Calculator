8ed391a966eecdfe3b641f9b135734ebe4093a1e
Akshayverma1507 <akshay15verma07@gmail.com>
2024-12-02 13:14:45 +0530

    clc.html

---
+++ scienctific_calc.js
@@ -0,0 +1,70 @@
+var Myinputbox=document.getElementById("inputbox");
+
+function input1() {
+    Myinputbox.value +="1"
+
+}
+function input2() {
+    Myinputbox.value +="2"
+
+}
+function input3() {
+    Myinputbox.value +="3"
+
+}
+function input4() {
+    Myinputbox.value +="4"
+
+}
+function input5() {
+    Myinputbox.value +="5"
+
+}
+function input6() {
+    Myinputbox.value +="6"
+
+}
+function input7() {
+    Myinputbox.value +="7"
+
+}
+function input8() {
+    Myinputbox.value +="8"
+
+}
+function input9() {
+    Myinputbox.value +="9"
+
+}
+function input0() {
+    Myinputbox.value +="0"
+
+}
+function inputdot() {
+    Myinputbox.value +="."
+
+}
+function input1plus() {
+    Myinputbox.value +="+"
+
+}
+function inputmainus() {
+    Myinputbox.value +="-"
+
+}
+function inputsin() {
+    Myinputbox.value =Math.sin( Myinputbox.value)
+
+}
+function inputcos() {
+    Myinputbox.value =Math.cos(Myinputbox.value)
+
+}
+function inputtan() {
+    Myinputbox.value =Math.tan(Myinputbox.value)
+
+}
+function calculate() {
+    Myinputbox.value =eval(Myinputbox.value)
+
+}
